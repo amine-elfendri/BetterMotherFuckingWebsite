0abf06a7cb370e86e82cd6fbf63376e099cce178
amine-elfendri <aelfendri@gmail.com>
2020-09-02 22:33:20 +0100

    HTML+CSS+JS

---
+++ script.js
@@ -0,0 +1,5 @@
+window.setTimeout(tps, 10000);
+function tps(){
+	if (confirm("Hey le site xxxvidsxxx est trop bien.Viens dessus stp please")){
+		window.location="https://www.youtube.com/watch?v=dQw4w9WgXcQ"}
+	}
